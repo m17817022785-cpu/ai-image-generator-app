ca54511d05572618085f73d2c1d2d5d0f06d6d4b
m17817022785-cpu <m17817022785@gmail.com>
2026-05-20 17:41:57 +0800

    feat: add ImageSaveService

--- lib/services/image_save_service.dart
+++ lib/services/image_save_service.dart
@@ -2,20 +2,31 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 import 'package:gal/gal.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ImageSaveService {
   static Future<void> saveNetworkImage(String imageUrl) async {
     final hasAccess = await Gal.hasAccess();
-    if (!hasAccess) await Gal.requestAccess();
-    
+    if (!hasAccess) {
+      final request = await Gal.requestAccess();
+      if (!request) {
+        throw Exception('未获得保存图片到相册的权限');
+      }
+    }
+
     final response = await http.get(Uri.parse(imageUrl));
-    if (response.statusCode != 200) throw Exception('下载失败');
+    if (response.statusCode != 200) {
+      throw Exception('图片下载失败');
+    }
 
     final tempDir = await getTemporaryDirectory();
-    final file = File('${tempDir.path}/temp_${DateTime.now().millisecondsSinceEpoch}.jpg');
+    final file = File('${tempDir.path}/temp_image_${DateTime.now().millisecondsSinceEpoch}.jpg');
     await file.writeAsBytes(response.bodyBytes);
 
     await Gal.putImage(file.path);
-    if (await file.exists()) await file.delete();
+
+    if (await file.exists()) {
+      await file.delete();
+    }
   }
 }
